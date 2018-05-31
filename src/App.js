9d967f62764993f19bda193ddc6dd1a28c5a64f6
mblaul <mattblaul@gmail.com>
2018-05-31 19:49:20 -0400

    Preredux!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {P}
+
+import Posts from "./Components/Posts";
+import Postform from "./Components/Postform";
 
 class App extends Component {
   render() {
@@ -10,9 +14,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Postform />
+        <hr />
+        <Posts />
       </div>
     );
   }
---
+++ src/Components/Postform.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+class Postform extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/posts`, { post })
+    .then(res => {
+      console.log(res.data);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title: </label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+          <div>
+            <label>Body: </label>
+            <br />
+            <textarea
+              name="body"
+              value={this.state.body}
+              onChange={this.onChange}
+            />
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Postform;
